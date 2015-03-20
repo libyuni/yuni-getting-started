7b092ccba24c367f98f1b979213d2136ff2da7d0
Damien GERARD <damien.gerard@4d.com>
2015-03-20 18:06:30 +0100

    added yuni/messaging

---
+++ src/ext/yuni/src/yuni/messaging/service.hxx
@@ -0,0 +1,58 @@
+#ifndef __YUNI_MESSAGING_SERVICE_HXX__
+# define __YUNI_MESSAGING_SERVICE_HXX__
+
+
+namespace Yuni
+{
+namespace Messaging
+{
+
+	inline Service::Transports::Transports()
+		: pService()
+	{}
+
+
+	inline Service::HeavyTasks::HeavyTasks()
+		: pService()
+	{}
+
+
+	inline Net::Error Service::Transports::add(const Net::Port& port, Transport::ITransport::Ptr transport)
+	{
+		return add("*", port, transport);
+	}
+
+
+	inline bool Service::HeavyTasks::enabled() const
+	{
+		return (0 != pEnabled);
+	}
+
+
+	inline void Service::HeavyTasks::enabled(bool on)
+	{
+		pEnabled = (on ? 1 : 0);
+	}
+
+
+	inline Service::HeavyTasks& Service::HeavyTasks::operator += (Job::IJob* job)
+	{
+		queue += job;
+		return *this;
+	}
+
+
+	inline Service::HeavyTasks& Service::HeavyTasks::operator += (Job::IJob::Ptr& job)
+	{
+		queue += job;
+		return *this;
+	}
+
+
+
+
+
+} // namespace Messaging
+} // namespace Yuni
+
+#endif // __YUNI_MESSAGING_SERVICE_HXX__
